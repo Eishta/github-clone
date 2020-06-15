2dc1966048db0c8fca6e8988c3c8edbba8a97872
Eishta <eishtamital@gmail.com>
2020-06-15 07:04:44 +0530

    Updates

---
+++ precache-manifest.b50334bfb90a5dc9ea9a1675894d3457.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6717a2247ad6cf2dd9bb16c919cc3433",
+    "url": "/Eishta/github-clone/index.html"
+  },
+  {
+    "revision": "2917d087639ce07bb72d",
+    "url": "/Eishta/github-clone/static/css/2.4f1ed363.chunk.css"
+  },
+  {
+    "revision": "c99e2e3da2dc2e6a2b1c",
+    "url": "/Eishta/github-clone/static/css/main.89e33527.chunk.css"
+  },
+  {
+    "revision": "2917d087639ce07bb72d",
+    "url": "/Eishta/github-clone/static/js/2.318ffeac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Eishta/github-clone/static/js/2.318ffeac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99e2e3da2dc2e6a2b1c",
+    "url": "/Eishta/github-clone/static/js/main.e344c31f.chunk.js"
+  },
+  {
+    "revision": "ab559395b4c211e32b0d",
+    "url": "/Eishta/github-clone/static/js/runtime-main.0525dadf.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Eishta/github-clone/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Eishta/github-clone/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Eishta/github-clone/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Eishta/github-clone/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Eishta/github-clone/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
